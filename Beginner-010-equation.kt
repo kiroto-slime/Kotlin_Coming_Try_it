3b9c4cace1eae69f32fa1eddbfd7a91d338dd208
Quartz Slimer <edward23242441@gmail.com>
2026-06-14 18:24:49 +0800

    Create Beginner-010-equation.kt

---
+++ Beginner-010-equation.kt
@@ -0,0 +1,25 @@
+package org.kotlin_Coming_Try_it
+import kotlin.math.sqrt
+
+fun main() {
+    val (a,b,c)= readln().split(" ").map {it.toDouble()}
+
+    val D= b * b - 4 * a * c
+
+    val ans= when {
+        D> 0-> {
+            val x1= (-b+ sqrt(D)) / (2 * a)
+            val x2= (-b- sqrt(D)) / (2 * a)
+
+            val small= minOf(x1, x2)
+            val big= maxOf(x1, x2)
+            "$small, $big"
+        }
+        D== 0.0-> {
+            val x= -b / (2 * a)
+            "DR=$x"
+        }
+        else -> "NoSolution"
+    }
+    println(ans)
+}
