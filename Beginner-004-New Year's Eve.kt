29555f751a3ac143044d873f32aa9a78e25da4dd
Quartz Slimer <edward23242441@gmail.com>
2026-06-14 17:15:49 +0800

    Update Beginner-004-New Year's Eve.kt

--- Beginner-004-New Year's Eve.kt
+++ Beginner-004-New Year's Eve.kt
@@ -3,7 +3,7 @@ package org.kotlin_Coming_Try_it
 fun main() {
     val year= readln().toInt()
     val ans: String
-    if ((year % 4 == 0 && year % 100 !=-0) || (year % 400 ==0)) {
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
         ans= "a leap year"
     }
     else {ans= "a normal year"}
