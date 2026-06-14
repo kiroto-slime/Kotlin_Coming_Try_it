a2089ea34ad4399c06a0392f04a9d18f2756a007
Quartz Slimer <edward23242441@gmail.com>
2026-06-14 16:54:51 +0800

    Create Beginner-002-Find the area and perimeter.kt

---
+++ Beginner-002-Find the area and perimeter.kt
@@ -0,0 +1,12 @@
+package org.kotlin_Coming_Try_it
+import kotlin.math.pow
+fun main() {
+    val x= readln().toFloat()
+    val pi= 3.14
+    if (x<0) {
+        println("Wrong")
+    }
+    else {
+        println("${x.pow(2) * pi} ${2 * pi * x}")
+    }
+}
