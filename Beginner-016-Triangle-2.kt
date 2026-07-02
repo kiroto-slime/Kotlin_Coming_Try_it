ae90b3a9eacef4233bd752b572d8a4de7f63f8fd
Quartz Slimer <edward23242441@gmail.com>
2026-07-02 09:51:41 +0800

    Update Beginner-016-Triangle-2.kt

--- Beginner-016-Triangle-2.kt
+++ Beginner-016-Triangle-2.kt
@@ -4,11 +4,11 @@ fun main() {
     val a = readln().toInt()
 
     for (i in 1..a) {
-        print("  ".repeat(a - i))
+        print("  ".repeat(a-i))
         for (e in 1..i) {
             print("$e ")
         }
-        for (g in i - 1 downTo 1) {
+        for (g in i-1 downTo 1) {
             print("$g ")
         }
         println()
