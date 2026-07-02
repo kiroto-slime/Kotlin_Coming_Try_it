ebc1c94db4ac9e273c28a6bf073acba1475c2c74
Quartz Slimer <edward23242441@gmail.com>
2026-07-02 09:51:12 +0800

    Create Beginner-016-Triangle-2.kt

---
+++ Beginner-016-Triangle-2.kt
@@ -0,0 +1,16 @@
+package org.kotlin_Coming_Try_it
+
+fun main() {
+    val a = readln().toInt()
+
+    for (i in 1..a) {
+        print("  ".repeat(a - i))
+        for (e in 1..i) {
+            print("$e ")
+        }
+        for (g in i - 1 downTo 1) {
+            print("$g ")
+        }
+        println()
+    }
+}
